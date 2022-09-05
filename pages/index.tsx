91b3bd2407f2170e647d6c15fb286b37d94904f1
Javier Szyfer <jszyfer@gmail.com>
2022-09-05 14:36:31 -0300

    nfts in wallet

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/common/Header";
 import InitView from "../components/InitView";
-import useSWR from "swr";
-import { getNFTsInWallet } from "../lib/OwnedNftsQuery";
-import request, { RequestDocument } from "graphql-request";
 
 const Home: NextPage = () => {
-  const fetcher = (query: RequestDocument) =>
-    request("https://api.zora.co/graphql", query);
-
-  const { data: nfts } = useSWR(getNFTsInWallet, fetcher, {
-    refreshInterval: 5,
-  });
-  console.log(nfts);
-
   return (
     <div className=" flex flex-col justify-center items-center overflow-hidden  ">
       <Head>
---
+++ pages/nfts.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import useDrop1Context from "../context/drop1Context/drop1Ctx";
+import { useAccount } from "wagmi";
+import Header from "../components/common/Header";
+
+const NftsInWallet = () => {
+  const { address } = useAccount();
+  //DROP 1
+  const { nftsInWallet } = useDrop1Context();
+  const tokens = nftsInWallet?.tokens.nodes;
+  console.log("nftsInWallet", tokens);
+
+  return (
+    <div className=" max-w-6xl mx-auto ">
+      <Head>
+        <title>FeltZine - Fjord</title>
+        <meta name="description" content="Lost echoes - nfts owned" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="noise"></div>
+      <Header />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-12">
+        {tokens?.map((t: any) => {
+          const token = t.token;
+          return (
+            <div
+              key={token.tokenId}
+              className="flex flex-col justify-start items-center  border border-[#f8f8f8] p-2 text-white w-full object-cover"
+            >
+              <img src={token.image.url} alt="owner's nfts" />
+              <div className=" mt-8 text-sm">Token id: {token.tokenId}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NftsInWallet;
