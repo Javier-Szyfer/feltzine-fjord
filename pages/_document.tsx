ca87fe4c89736d3742eea3e34466d9dc11b634e0
Javier Szyfer <jszyfer@gmail.com>
2022-09-11 16:40:23 -0300

    default-seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=5"
-        />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link
           rel="icon"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Header from "../components/common/Header";
 import InitView from "../components/InitView";
 
 const Home: NextPage = () => {
   return (
     <div className=" flex flex-col justify-center items-center overflow-hidden  ">
-      <Head>
-        <meta
-          property="og:url"
-          content="https://res.cloudinary.com/aldi/image/upload/v1662551284/feltzine/preview_nsva8h.jpg"
-        />
-        <meta property="og:site_name" content="FELT Zine x FJORD" />
-        <meta property="og:title" content="FELT Zine x FJORD NFT" />
-        <meta
-          property="og:description"
-          content="Revolutionizing NFT Drops With Felt Zine and Fjord NFTs Liquidity Bootstrapping Pools "
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/aldi/image/upload/v1662551284/feltzine/preview_nsva8h.jpg"
-          key="ogimage"
-        />
-        <meta property="twitter:site" content="FeltZine" />
-        <meta property="twitter:title" content="FELT Zine x FJORD NFT" />
-        <meta
-          property="twitter:description"
-          content="Revolutionizing NFT Drops With Felt Zine and Fjord NFTs Liquidity Bootstrapping Pools "
-        />
-        <meta
-          property="twitter:image:src"
-          content="https://res.cloudinary.com/aldi/image/upload/v1662551284/feltzine/preview_nsva8h.jpg"
-        />
-        <meta property="twitter:creator" content="FeltZine" />
-        <meta name="twitter:card" content="summary_large_image" key="twcard" />
-      </Head>
       <div className="noise" />
       <Header />
       <InitView />
