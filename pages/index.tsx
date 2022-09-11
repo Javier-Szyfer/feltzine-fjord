4921ddec582b0bc09c32ec6aefe860e6d0bbf4fc
Javier Szyfer <jszyfer@gmail.com>
2022-09-11 16:06:46 -0300

    ogg

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,10 @@ const Home: NextPage = () => {
   return (
     <div className=" flex flex-col justify-center items-center overflow-hidden  ">
       <Head>
-        <meta property="og:url" content="https://fjord.feltzine.art" />
+        <meta
+          property="og:url"
+          content="https://res.cloudinary.com/aldi/image/upload/v1662551284/feltzine/preview_nsva8h.jpg"
+        />
         <meta property="og:site_name" content="FELT Zine x FJORD" />
         <meta property="og:title" content="FELT Zine x FJORD NFT" />
         <meta
@@ -17,13 +20,10 @@ const Home: NextPage = () => {
         <meta
           property="og:image"
           content="https://res.cloudinary.com/aldi/image/upload/v1662551284/feltzine/preview_nsva8h.jpg"
+          key="ogimage"
         />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-
         <meta property="twitter:site" content="FeltZine" />
         <meta property="twitter:title" content="FELT Zine x FJORD NFT" />
-        <meta property="twitter:domain" content="fjord.feltzine.art" />
         <meta
           property="twitter:description"
           content="Revolutionizing NFT Drops With Felt Zine and Fjord NFTs Liquidity Bootstrapping Pools "
@@ -32,10 +32,8 @@ const Home: NextPage = () => {
           property="twitter:image:src"
           content="https://res.cloudinary.com/aldi/image/upload/v1662551284/feltzine/preview_nsva8h.jpg"
         />
-        <meta property="twitter:image:width" content="1200" />
-        <meta property="twitter:image:height" content="630" />
         <meta property="twitter:creator" content="FeltZine" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" key="twcard" />
       </Head>
       <div className="noise" />
       <Header />
