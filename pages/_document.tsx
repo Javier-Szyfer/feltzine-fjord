9be076148b52265306f475db90f692053a204dd4
Javier Szyfer <jszynet@gmail.com>
2022-09-17 18:35:02 -0300

    og11

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,15 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const title = 'FeltZine x Fjord';
+  const description =
+    'Revolutionizing NFT Drops With Felt Zine and Fjord NFTs Liquidity Bootstrapping Pools';
+  const image = 'https://www.feltzine-fjord.vercel.app/assets/general-og.jpg';
+
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel="icon" href="https://fjord.feltzine.art/assets/favicon.jpg" />
         <meta content="#000000" name="theme-color" />
@@ -16,6 +20,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed&family=Public+Sans:ital,wght@1,200&family=VT323&display=swap"
           rel="stylesheet"
         />
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://feltzine-fjord.vercel.app" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="Feltzine" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+        <meta property="twitter:creator" content="FeltZine" />
+        <meta property="og:image:alt" content={`${title}- open graph card`} />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,10 @@ import Header from '../components/common/Header';
 import InitView from '../components/InitView';
 
 const Home: NextPage = () => {
-  const title = 'FeltZine x Fjord';
-  const description =
-    'Revolutionizing NFT Drops With Felt Zine and Fjord NFTs Liquidity Bootstrapping Pools';
-  const image = 'https://www.feltzine-fjord.vercel.app/assets/general-og.jpg';
-
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://feltzine-fjord.vercel.app" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:site" content="Feltzine" />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
-        <meta property="twitter:creator" content="FeltZine" />
-        <meta property="og:image:alt" content={`${title}- open graph card`} />
+        <title>FeltZine x Fjord</title>
       </Head>
 
       <div className="flex flex-col justify-center items-center overflow-hidden  ">
